b10ac1ac5d81112dd59e008fbdfda8a44e02f106
areeb <areebulhaq2005@gmail.com>
2026-03-01 23:15:04 +0500

    Add contact management views and styles
    - Created custom CSS file for styling the application.
    - Added 'addContact.ejs' for adding new contacts with a form.
    - Implemented 'home.ejs' to display all contacts in a table format with action buttons.
    - Created 'viewContact.ejs' to show details of a selected contact.
    - Placeholder for 'updateContact.ejs' added for future implementation.

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ let app = express();
 app.listen(3000,()=>{
     console.log("Server has started");
 });
-
-__
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:false}));
+app.set("view engine","ejs");
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
+});
+app.get("/addContact",(req,res)=>{
+    res.render("addContact.ejs");
+});
+app.get("/viewContact",(req,res)=>{
+    res.render("viewContact.ejs");
+});
