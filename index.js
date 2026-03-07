62a1b7afc649c0dd2c4e4a28c623f8929ae49c0f
areeb <areebulhaq2005@gmail.com>
2026-03-08 04:13:31 +0500

    fixed a small bug in the eng file that was causing the server to crash

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
 import {users} from "./models/users.models.js";
 import {connectDB} from "./config/contacts.database.js";
 import contactRouter from "./routers/contacts.routes.js";
 let app = express();
-dotenv.config();
+
 connectDB();
 app.listen(process.env.PORT,()=>{
     console.log("Server has started");
@@ -13,4 +14,4 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.set("view engine","ejs");
-app.use(contactRouter);
+app.use("/" , contactRouter);
